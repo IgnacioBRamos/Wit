9609fa33804af3912112dd5bbf9411bb86f9b853
Ignacio Ramos <ignaciobenjamin.ramos@gmail.com>
2025-02-04 22:06:54 -0300

    cambios

--- script.js
+++ script.js
@@ -117,25 +117,25 @@ close.addEventListener("click",()=>{
 
 
 
-const scrollers = document.querySelectorAll(".scroller");
+// const scrollers = document.querySelectorAll(".scroller");
 
-if(!window.matchMedia("(prefers-reduced-motion:reduce)").matches){
-    addAnimation()
-}
+// if(!window.matchMedia("(prefers-reduced-motion:reduce)").matches){
+//     addAnimation()
+// }
 
 
-function addAnimation(){
-    scrollers.forEach(scroller=>{
-        scroller.setAttribute("data-animated",true);
+// function addAnimation(){
+//     scrollers.forEach(scroller=>{
+//         scroller.setAttribute("data-animated",true);
         
-        const scrollerInner = scroller.querySelector(".scroller-inner")
-        const scrollerContent = Array.from(scrollerInner.children)
+//         const scrollerInner = scroller.querySelector(".scroller-inner")
+//         const scrollerContent = Array.from(scrollerInner.children)
 
-        scrollerContent.forEach(item =>{
-            const duplicatedItem = item.cloneNode(true);
-            duplicatedItem.setAttribute("aria-hidden",true)
-            scrollerInner.appendChild(duplicatedItem)
-        })
+//         scrollerContent.forEach(item =>{
+//             const duplicatedItem = item.cloneNode(true);
+//             duplicatedItem.setAttribute("aria-hidden",true)
+//             scrollerInner.appendChild(duplicatedItem)
+//         })
 
-    })
-}
+//     })
+// }
