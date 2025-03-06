2726df8f12c2993f75b0f5930d412766044faafb
Ignacio Ramos <ignaciobenjamin.ramos@gmail.com>
2025-03-06 18:17:40 -0300

    Cambios el footer donde estaban las brands y las pusimos al lador del main text como un slider

--- script.js
+++ script.js
@@ -6,12 +6,24 @@ $(document).ready(function(){
         autoplaySpeed: 5000,
         slideBy: 1,   
         margin: 15, /* Espacio entre los elementos */
-        nav: true,
         responsive: {
             0: { items: 1 },
-            600: { items: 2 },
-            1000: { items: 3 }
+            700: { items: 2 },
+            1440: { items: 3 }
         }
+    });
+});
+
+$(document).ready(function(){
+    $('.brands-carousel').owlCarousel({
+        items:1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 5000,
+        slideBy: 1,   
+        margin: 15, /* Espacio entre los elementos */
+       
     });
 });
 
@@ -19,65 +31,65 @@ $(document).ready(function(){
 
 
 
-const form = document.getElementById("formulario")
+// const form = document.getElementById("formulario")
 
-form.addEventListener("submit",handlesubmit)
+// form.addEventListener("submit",handlesubmit)
 
-async function handlesubmit(event){
-    event.preventDefault()
-    let email = document.getElementById("email").value
-    let phone = document.getElementById("phone").value
+// async function handlesubmit(event){
+//     event.preventDefault()
+//     let email = document.getElementById("email").value
+//     let phone = document.getElementById("phone").value
     
-    let regexEmail = /[\w.-]+@[\w]+\.\w{2,}/g
-    let regexPhone = /^\+?\d+$/g
+//     let regexEmail = /[\w.-]+@[\w]+\.\w{2,}/g
+//     let regexPhone = /^\+?\d+$/g
 
-    if(!regexEmail.test(email)){
+//     if(!regexEmail.test(email)){
         
-        Toastify({
-            text: "Please enter a valid email address.",
-            className: "warning",
-            style: {
-              background: "red",
-            }
-          }).showToast();
-        return false
-    }
-    if(!regexPhone.test(phone)){
-        // alert("Please enter a valid number without dashes or spaces. The area code is optional.")
-        Toastify({
-            text: "Please enter a valid number without dashes or spaces. The area code is optional.",
-            className: "warning",
-            style: {
-              background: "red",
-            }
-          }).showToast();
-        return false
-    }
-    const form = new FormData(this)
+//         Toastify({
+//             text: "Please enter a valid email address.",
+//             className: "warning",
+//             style: {
+//               background: "red",
+//             }
+//           }).showToast();
+//         return false
+//     }
+//     if(!regexPhone.test(phone)){
+//         // alert("Please enter a valid number without dashes or spaces. The area code is optional.")
+//         Toastify({
+//             text: "Please enter a valid number without dashes or spaces. The area code is optional.",
+//             className: "warning",
+//             style: {
+//               background: "red",
+//             }
+//           }).showToast();
+//         return false
+//     }
+//     const form = new FormData(this)
     
-    const response = await fetch(this.action,{
-        method:this.method,
-        body:form,
-        headers:{
-            "accept": "application/json"
-        }
-    })
-    if (response.ok){
-        this.reset()
+//     const response = await fetch(this.action,{
+//         method:this.method,
+//         body:form,
+//         headers:{
+//             "accept": "application/json"
+//         }
+//     })
+//     if (response.ok){
+//         this.reset()
         
-        // event.target.classList.add("envioformulario");
-        // setTimeout(()=>{
-        //     event.target.classList.remove("envioformulario");
-        // },5000)
-        Toastify({
-            text: "Thank you for reaching out. I will get back to you shortly.",
-            className: "success",
-            style: {
-              background: "green",
-            }
-          }).showToast();
-    }
-}
+//         // event.target.classList.add("envioformulario");
+//         // setTimeout(()=>{
+//         //     event.target.classList.remove("envioformulario");
+//         // },5000)
+//         Toastify({
+//             text: "Thank you for reaching out. I will get back to you shortly.",
+//             className: "success",
+//             style: {
+//               background: "green",
+//             }
+//           }).showToast();
+//     }
+// }
 
 
 
