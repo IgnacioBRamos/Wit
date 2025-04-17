44e181c997873efb324eed4fb89144f8e305806f
Ignacio Ramos <ignaciobenjamin.ramos@gmail.com>
2025-04-17 16:55:24 -0300

    se agrego la pagina de privacy policy y agrego medidas a las imagenes de los logos de las brands

--- script3.js
+++ script3.js
@@ -1,90 +1,66 @@
 const nav = document.querySelector("#nav");
-        const open = document.querySelector("#open");
-        const close = document.querySelector("#close");
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
 
 
 
-        open.addEventListener("click", () => {
+open.addEventListener("click", () => {
 
-            nav.classList.add("visible");
-        })
+    nav.classList.add("visible");
+})
 
-        close.addEventListener("click", () => {
+close.addEventListener("click", () => {
 
-            nav.classList.remove("visible");
-        })
-
-
-
-
+    nav.classList.remove("visible");
+})
 
 
 
 
 
-        const form = document.getElementById("formulario")
 
-        form.addEventListener("submit",handlesubmit)
-        
-        async function handlesubmit(event){
-            event.preventDefault()
-            let email = document.getElementById("email").value
-            let phone = document.getElementById("phone").value
-            
-            let regexEmail = /[\w.-]+@[\w]+\.\w{2,}/g
-            let regexPhone = /^\+?\d+$/g
-        
-            if(!regexEmail.test(email)){
-                alert("Please enter a valid email address.")
-                return false
-            }
-            if(!regexPhone.test(phone)){
-                alert("Please enter a valid number without dashes or spaces. The area code is optional.")
-                return false
-            }
-            const form = new FormData(this)
-            
-            const response = await fetch(this.action,{
-                method:this.method,
-                body:form,
-                headers:{
-                    "accept": "application/json"
-                }
-            })
-            if (response.ok){
-                this.reset()
-                console.log(form)
-                alert("Thank you for reaching out. I will get back to you shortly.")
-            }
+
+
+
+const form = document.getElementById("formulario")
+
+form.addEventListener("submit", handlesubmit)
+
+async function handlesubmit(event) {
+    event.preventDefault()
+    let email = document.getElementById("email").value
+    let phone = document.getElementById("phone").value
+
+    let regexEmail = /[\w.-]+@[\w]+\.\w{2,}/g
+    let regexPhone = /^\+?\d+$/g
+
+    if (!regexEmail.test(email)) {
+        alert("Please enter a valid email address.")
+        return false
+    }
+    if (!regexPhone.test(phone)) {
+        alert("Please enter a valid number without dashes or spaces. The area code is optional.")
+        return false
+    }
+    const form = new FormData(this)
+
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            "accept": "application/json"
         }
-
-
-
-
-
-
-
-
-
-        const scrollers = document.querySelectorAll(".scroller");
-
-if(!window.matchMedia("(prefers-reduced-motion:reduce)").matches){
-    addAnimation()
-}
-
-
-function addAnimation(){
-    scrollers.forEach(scroller=>{
-        scroller.setAttribute("data-animated",true);
-        
-        const scrollerInner = scroller.querySelector(".scroller-inner")
-        const scrollerContent = Array.from(scrollerInner.children)
-
-        scrollerContent.forEach(item =>{
-            const duplicatedItem = item.cloneNode(true);
-            duplicatedItem.setAttribute("aria-hidden",true)
-            scrollerInner.appendChild(duplicatedItem)
-        })
-
     })
+    if (response.ok) {
+        this.reset()
+        console.log(form)
+        alert("Thank you for reaching out. I will get back to you shortly.")
+    }
 }
+
+
+
+
+
+
+
