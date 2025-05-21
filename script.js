00dc5ad6de4289f731c4823865347bd4fd436455
Ignacio Ramos <ignaciobenjamin.ramos@gmail.com>
2025-05-20 22:43:29 -0300

    Se agrego slide de brands con el fin de que el cliente vea las marcas que nos apoyan. Se creo un navbar con dropdown menu, para poder navegar entre servicios, tambien se lo hizo responsive

--- script.js
+++ script.js
@@ -1,38 +1,86 @@
-const openButton = document.getElementById('open-sidebar-button');
-const navbar = document.getElementById('navbar');
-const media = window.matchMedia("(width < 700px)");
 
 
-media.addEventListener('change',(e)=>updateNavbar(e));
 
-function updateNavbar(e){
-  const isMobile = e.matches;
-  console.log(isMobile);
 
-  if(isMobile){
-    navbar.setAttribute('inert','');
+
+
+
+
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach(scroller => {
+    scroller.setAttribute('data-animated', true);
+    const scrollerInner = scroller.querySelector('.scroller__inner');
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach(item => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+
+    })
+  })
+}
+
+
+
+
+
+(() => {
+
+  const openNav = document.querySelector(".open-menu"),
+    closeNav = document.querySelector(".close-menu"),
+    navMenu = document.querySelector(".nav-links-container"),
+    overlay = document.querySelector(".overlay"),
+    mediaSize = 992;
+
+  openNav.addEventListener("click", toggleMenu);
+  closeNav.addEventListener("click", toggleMenu);
+  overlay.addEventListener("click",toggleMenu);
+
+
+  function toggleMenu() {
+    navMenu.classList.toggle("open");
+    overlay.classList.toggle("active");
   }
-  else{
-    navbar.removeAttribute('inert');
+
+
+
+  navMenu.addEventListener("click", (event) => {
+
+
+    if (event.target.hasAttribute("data-toggle") && window.innerWidth <= mediaSize) {
+      event.preventDefault();
+      const dropdownMenuBranch = event.target.parentElement;
+
+
+      if (dropdownMenuBranch.classList.contains("active")) {
+        collapseDropdownMenu();
+      }
+
+      else {
+        if (navMenu.querySelector(".dropdown-menu-branch.active")) {
+          collapseDropdownMenu();
+        }
+        dropdownMenuBranch.classList.add("active");
+        const dropdownMenu = dropdownMenuBranch.querySelector(".dropdown-menu");
+        dropdownMenu.style.maxHeight = dropdownMenu.scrollHeight + "px";
+
+      }
+    }
+  });
+
+  function collapseDropdownMenu(){
+    navMenu.querySelector(".dropdown-menu-branch.active .dropdown-menu").removeAttribute("style");
+    navMenu.querySelector(".dropdown-menu-branch.active").classList.remove("active");
   }
 
-}
-
-function openSidebar(){
-  navbar.classList.add('show');
-  openButton.setAttribute('aria-expanded','true');
-  navbar.removeAttribute('inert');
-}
-
-function closeSidebar(){
-  navbar.classList.remove('show');
-  navbar.setAttribute('inert','');
-}
-
-updateNavbar(media);
 
 
 
-
-
-
+})();
