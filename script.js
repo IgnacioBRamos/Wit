0adb6c46a595c57951ee1be93acc38f7350e86b3
Ignacio Ramos <ignaciobenjamin.ramos@gmail.com>
2025-05-28 09:26:35 -0300

    Cambio en iconos de los servicios con hover, por estetica, y cambio en tamanio de imagen logo wit

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".nav-container");
+
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
 
 
 
